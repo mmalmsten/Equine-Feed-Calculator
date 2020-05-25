76578927b7e939e797e55da5707291557b135864
Madeleine Malmsten <madeleine_malmsten@mckinsey.com>
2020-05-25 21:51:30 +0200

    Refactoring

--- feed.pl
+++ feed.pl
@@ -1,44 +1,71 @@
-hay(Dict, {Energy, Protein, Calcium, Phosphorus, Magnesium}, Amount, {Energy1, Protein1, Calcium1, Phosphorus1, Magnesium1}) :-
-    numlist(1, 10, L), reverse(L, L1), member(Amount, L1),
-    Amount >= (Dict.weight / 100) / Dict.hay.ts,
-    Energy1 is Energy - Dict.hay.energy * Amount * Dict.hay.ts,
-    Energy1 >= 0,
-    Protein1 is Protein - Dict.hay.protein * Amount * Dict.hay.ts,
-    Calcium1 is Calcium - Dict.hay.calcium * Amount * Dict.hay.ts,
-    Phosphorus1 is Phosphorus - Dict.hay.phosphorus * Amount * Dict.hay.ts,
-    Magnesium1 is Magnesium - Dict.hay.magnesium * Amount * Dict.hay.ts.
+feed(Id) :-
+    findnsols(1000, {HayKg1, Amount, Grains}, (
+        hay(Id, Hay), 
+        grains(Id, Hay, Grains), 
+        member(kg=HayKg, Hay), HayKg1 is 0 - HayKg,
+        Amount = Grains.totalGrain
+    ), Bag),
+    sort(Bag, Bag1), [{HayK,_,Grain}|_] = Bag1, HayK1 is 0 - HayK,
+    update_horse(Id, [haykg=HayK1, grains=Grain]).
 
-grains({Energy, Protein, Calcium, Phosphorus, Magnesium}, [[Amount1, Name1], [Amount2, Name2]], {Energy1, Protein1, Calcium1, Phosphorus1, Magnesium1}) :-
-    csv_read_file('krafft.csv', Feed, [functor(feed)]),
-    member(feed(Name1,Ts1,FEn1,FPr1,_FL1,FCs1,FP1,FM1), Feed),
-    member(feed(Name2,Ts2,FEn2,FPr2,_FL2,FCs2,FP2,FM2), Feed),
-    numlist(0, 20, L), member(Amount1, L), member(Amount2, L),
-    (Name1 \= Name2 ; Amount1 = 0),
-    Energy1 is Energy - (Amount1 * (FEn1 / 10)) - (Amount2 * (FEn2 / 10)),
-    Energy1 > -5, 
-    Energy1 < 5,
-    Protein1 is Protein - (Amount1 * (FPr1 / 10)) - (Amount2 * (FPr2 / 10)),
-    Protein1 =< 0,
-    Calcium1 is Calcium - (Amount1 * (FCs1 / 10)) - (Amount2 * (FCs2 / 10)),
-    Calcium1 < 0,
-    Phosphorus1 is Phosphorus - (Amount1 * (FP1 / 10)) - (Amount2 * (FP2 / 10)),
-    Phosphorus1 < 0,
-    Magnesium1 is Magnesium - (Amount1 * (FM1 / 10)) - (Amount2 * (FM2 / 10)),
-    Magnesium1 < 0.
+%-------------------------------------------------------------------------------
+% Amount of hay
+%-------------------------------------------------------------------------------
+hay(Id, [kg=Kg, mj=Mj, smbrp=SmbRp, ca=Ca, p=P, mg=Mg]) :-
+    horse(Id, Dict),
+    % Hay amounts
+    Max is ceil(5 * (Dict.weight / 100) / Dict.hay.ts),
+    Min is floor((Dict.weight / 100) / Dict.hay.ts),
+    numlist(Min, Max, L), reverse(L, L1), member(Kg, L1),
+    % Aggregate
+    Mj is Dict.hay.mj * Kg * Dict.hay.ts, Mj =< Dict.mj,
+    SmbRp is Dict.hay.smbrp * Kg * Dict.hay.ts,
+    Ca is Dict.hay.ca * Kg * Dict.hay.ts,
+    P is Dict.hay.p * Kg * Dict.hay.ts,
+    Mg is Dict.hay.mg * Kg * Dict.hay.ts.
 
-feed(Dict, Energy, Protein, Calcium, Phosphorus, Magnesium, [[hay, Amount], [Amount1, Name1], [Amount2, Name2]], Energy3, Protein3, Calcium3, Phosphorus3, Magnesium3) :-
-    hay(Dict, {Energy, Protein, Calcium, Phosphorus, Magnesium}, Amount, {Energy1, Protein1, Calcium1, Phosphorus1, Magnesium1}),
-    % format('~p kg hay~n',[Amount]),
-    grains({Energy1, Protein1, Calcium1, Phosphorus1, Magnesium1}, [[Amount1, Name1], [Amount2, Name2]], {Energy2, Protein2, Calcium2, Phosphorus2, Magnesium2}),
-    Energy3 is Energy - Energy2,
-    Protein3 is Protein - Protein2,
-    Calcium3 is Calcium - Calcium2,
-    Phosphorus3 is Phosphorus - Phosphorus2,
-    Magnesium3 is Magnesium - Magnesium2,
-    Calcium3 / Phosphorus3 > 1.1,
-    Protein3 / Energy3 > 6.
-    % format('Energy ~p~n',[[Energy, Energy3]]),
-    % format('Protein ~p~n',[[Protein, Protein3]]),
-    % format('Calcium ~p~n',[[Calcium, Calcium3]]),
-    % format('Phosphorus ~p~n',[[Phosphorus, Phosphorus3]]),
-    % format('Magnesium ~p~n',[[Magnesium, Magnesium3]]),
+
+%-------------------------------------------------------------------------------
+% Find combination of grains
+%-------------------------------------------------------------------------------
+grains(Id, [kg=_, mj=HayMj, smbrp=HaySmbRp, ca=HayCa, p=HayP, mg=HayMg], Grains) :-
+    horse(Id, Dict),
+    csv_read_file('svenskafoder.csv', Feed1, [functor(feed)]),
+    csv_read_file('granngarden.csv', Feed2, [functor(feed)]),
+    csv_read_file('krafft.csv', Feed3, [functor(feed)]),
+    flatten([Feed1, Feed2, Feed3], Feed),
+    % Find 3 feeds
+    numlist(0, 20, L),
+    member(feed(Name1, Mj1, SmbRp1, _FL1, Ca1, P1, Mg1, MaxAmount1), Feed), member(Hg1, L),
+    member(feed(Name2, Mj2, SmbRp2, _FL2, Ca2, P2, Mg2, MaxAmount2), Feed), member(Hg2, L),
+    (Name1 \= Name2 ; Hg2 = 0),
+    member(feed(Name3, Mj3, SmbRp3, _FL3, Ca3, P3, Mg3, MaxAmount3), Feed), member(Hg3, L),
+    (Name1 \= Name3, Name2 \= Name3 ; Hg3 = 0),
+    Kg1 is Hg1/10, Kg2 is Hg2/10, Kg3 is Hg3/10,
+
+    % Ensure not too much
+    Kg1 =< MaxAmount1 * (Dict.weight / 100),
+    Kg2 =< MaxAmount2 * (Dict.weight / 100),
+    Kg3 =< MaxAmount3 * (Dict.weight / 100),
+    TotalGrain is Hg1 + Hg2 + Hg3,
+    TotalGrain =< (0.4 * (Dict.weight / 100) * 5) * 10,
+    
+    % Aggregate
+    Mj is HayMj + (Mj1 * Kg1) + (Mj2 * Kg2) + (Mj3 * Kg3),
+    Mj > Dict.mj - 2, Mj < Dict.mj + 2,
+
+    SmbRp is HaySmbRp + (SmbRp1 * Kg1) + (SmbRp2 * Kg2) + (SmbRp3 * Kg3),
+    SmbRp > Dict.smbRp,
+    % ((HaySmbRp > Dict.smbRp, SmbRpMax is HaySmbRp * 1.5) ; SmbRpMax is Dict.smbRp * 1.5),
+    % SmbRp < SmbRpMax,
+    SmbRp / Mj > 6,
+
+    Ca is HayCa + (Ca1 * Kg1) + (Ca2 * Kg2) + (Ca3 * Kg3), Ca > Dict.ca,
+    P is HayP + (P1 * Kg1) + (P2 * Kg2) + (P3 * Kg3), P > Dict.p,
+    Ca / P > 1.1,
+    HayMg + (Mg1 * Kg1) + (Mg2 * Kg2) + (Mg3 * Kg3) > Dict.mg,
+    
+    dict_create(Grain1, _, [hg=Hg1, name=Name1, mj=Mj1, smbrp=SmbRp1, ca=Ca1, p=P1, mg=Mg1]),
+    dict_create(Grain2, _, [hg=Hg2, name=Name2, mj=Mj2, smbrp=SmbRp2, ca=Ca2, p=P2, mg=Mg2]),
+    dict_create(Grain3, _, [hg=Hg3, name=Name3, mj=Mj3, smbrp=SmbRp3, ca=Ca3, p=P3, mg=Mg3]),
+    dict_create(Grains, grains, [totalGrain = TotalGrain, grain1=Grain1, grain2=Grain2, grain3=Grain3]).
--- nutrition.pl
+++ nutrition.pl
@@ -1,44 +1,110 @@
-energy(Age, Weight, Energy) :- Age =< 6, pow(Weight, 0.75, Pow), Energy is 0.63 * Pow.
-energy(Age, Weight, Energy) :- Age =< 12, pow(Weight, 0.75, Pow), Energy is 0.59 * Pow.
-energy(Age, Weight, Energy) :- Age =< 36, pow(Weight, 0.75, Pow), Energy is 0.57 * Pow.
-energy(_, Weight, Energy) :- pow(Weight, 0.75, Pow), Energy is 0.5 * Pow.
-    
-additional(false, _, 0).
-additional(_, Energy, (Energy * 0.1)).
+nutrition(Id) :-
+    mj(Id, Mj), update_horse(Id, Mj),
+    smbrp(Id, SmbRp), update_horse(Id, SmbRp),
+    ca(Id, Ca), update_horse(Id, Ca),
+    p(Id, P), update_horse(Id, P),
+    mg(Id, Mg), update_horse(Id, Mg).
 
-training(Walk, TrotCanter, Weight, Energy) :- 
-    Energy is ((Walk * 0.02 * (Weight / 100)) + (TrotCanter * 0.13 * (Weight / 100))).
+%-------------------------------------------------------------------------------
+% Energy need
+%-------------------------------------------------------------------------------
+mj(Id, [mj=Result,trainingMj=Training]) :- 
+    horse(Id, Dict),
+    mj_age(Dict.age, Factor), 
+    pow(Dict.weight, 0.75, Pow), 
+    Mj is Factor * Pow,
+    mj_extra(Dict.stallion, Mj, Mj1),
+    mj_extra(Dict.group, Mj, Mj2),
+    mj_training(Id, Mj3),
+    Result is Mj + Mj1 + Mj2 + Mj3,
+    Training is Mj3 / Mj.
 
-protein(Age, Energy, Protein) :- Age =< 4, Protein is Energy * 13.
-protein(Age, Energy, Protein) :- Age =< 6, Protein is Energy * 10.
-protein(Age, Energy, Protein) :- Age =< 12, Protein is Energy * 8.5.
-protein(Age, Energy, Protein) :- Age =< 18, Protein is Energy * 7.
-protein(Age, Energy, Protein) :- Age =< 36, Protein is Energy * 6.5.
-protein(_, Energy, Protein) :- Protein is Energy * 6.
+% Energy need based on age
+mj_age(Age, 0.63) :- Age =< 6.
+mj_age(Age, 0.59) :- Age =< 12.
+mj_age(Age, 0.57) :- Age =< 36.
+mj_age(_, 0.5).
 
-calcium(Age, Weight, _, Calcium) :- Age =< 6, Calcium is 17.2 * (Weight / 100).
-calcium(Age, Weight, _, Calcium) :- Age =< 12, Calcium is 10.3 * (Weight / 100).
-calcium(Age, Weight, _, Calcium) :- Age =< 24, Calcium is 7.5 * (Weight / 100).
-calcium(Age, Weight, _, Calcium) :- Age =< 36, Calcium is 6.2 * (Weight / 100).
-calcium(_, Weight, 0, Calcium) :- Calcium is 4 * (Weight / 100).
-calcium(_, Weight, Training, Calcium) :- Training >= 0.50, Calcium is 8 * (Weight / 100).
-calcium(_, Weight, Training, Calcium) :- Training >= 0.30, Calcium is 7 * (Weight / 100).
-calcium(_, Weight, _, Calcium) :- Calcium is 6 * (Weight / 100).
+% +10% energy need based on extra
+mj_extra(false, _, 0).
+mj_extra(_, Mj, (Mj * 0.1)).
 
-phosphorus(Age, Weight, _, Phosphorus) :- Age =< 6, Phosphorus is 9.6 * (Weight / 100).
-phosphorus(Age, Weight, _, Phosphorus) :- Age =< 12, Phosphorus is 5.7 * (Weight / 100).
-phosphorus(Age, Weight, _, Phosphorus) :- Age =< 24, Phosphorus is 4.2 * (Weight / 100).
-phosphorus(Age, Weight, _, Phosphorus) :- Age =< 36, Phosphorus is 3.4 * (Weight / 100).
-phosphorus(_, Weight, 0, Phosphorus) :- Phosphorus is 2.8 * (Weight / 100).
-phosphorus(_, Weight, Training, Phosphorus) :- Training >= 0.50, Phosphorus is 5.8 * (Weight / 100).
-phosphorus(_, Weight, Training, Phosphorus) :- Training >= 0.30, Phosphorus is 4.2 * (Weight / 100).
-phosphorus(_, Weight, _, Phosphorus) :- Phosphorus is 3.6 * (Weight / 100).
+% Energy need based on training
+mj_training(Id, Mj) :-
+    horse(Id, Dict),
+    Mj is ((Dict.walk * 0.02 * (Dict.weight / 100)) + 
+        (Dict.trotCanter * 0.13 * (Dict.weight / 100))).
 
-magnesium(Age, Weight, _, Magnesium) :- Age =< 6, Magnesium is 1.2 * (Weight / 100).
-magnesium(Age, Weight, _, Magnesium) :- Age =< 12, Magnesium is 1.7 * (Weight / 100).
-magnesium(Age, Weight, _, Magnesium) :- Age =< 24, Magnesium is 1.6 * (Weight / 100).
-magnesium(Age, Weight, _, Magnesium) :- Age =< 36, Magnesium is 1.5 * (Weight / 100).
-magnesium(_, Weight, 0, Magnesium) :- Magnesium is 1.5 * (Weight / 100).
-magnesium(_, Weight, Training, Magnesium) :- Training >= 0.50, Magnesium is 3 * (Weight / 100).
-magnesium(_, Weight, Training, Magnesium) :- Training >= 0.30, Magnesium is 2.3 * (Weight / 100).
-magnesium(_, Weight, _, Magnesium) :- Magnesium is 1.9 * (Weight / 100).
+%-------------------------------------------------------------------------------
+% Protein need
+%-------------------------------------------------------------------------------
+smbrp(Id, [smbRp=SmbRp]) :- 
+    horse(Id, Dict), smbrp_age(Dict.age, Factor), SmbRp is Dict.mj * Factor.
+
+% Protein need based on age
+smbrp_age(Age, 13) :- Age =< 4.
+smbrp_age(Age, 10) :- Age =< 6.
+smbrp_age(Age, 8.5) :- Age =< 12.
+smbrp_age(Age, 7) :- Age =< 18.
+smbrp_age(Age, 6.5) :- Age =< 36.
+smbrp_age(_, 6).
+
+%-------------------------------------------------------------------------------
+% Calcium need
+%-------------------------------------------------------------------------------
+ca(Id, [ca=Ca]) :- 
+    horse(Id, Dict), 
+    (ca_age(Dict.age, Factor) ; ca_training(Dict.trainingMj, Factor)),
+    Ca is Factor * (Dict.weight / 100).
+
+% Calcium need based on age
+ca_age(Age, 17.2) :- Age =< 6.
+ca_age(Age, 10.3) :- Age =< 12.
+ca_age(Age, 7.5) :- Age =< 24.
+ca_age(Age, 6.2) :- Age =< 36.
+
+% Calcium need based on training
+ca_training(0, 4).
+ca_training(Training, 8) :- Training >= 0.50.
+ca_training(Training, 7) :- Training >= 0.30.
+ca_training(_, 6).
+
+%-------------------------------------------------------------------------------
+% Phosphorus need
+%-------------------------------------------------------------------------------
+p(Id, [p=P]) :-
+    horse(Id, Dict),
+    (p_age(Dict.age, Factor) ; p_training(Dict.trainingMj, Factor)),
+    P is Factor * (Dict.weight / 100).
+
+% Phosphorus need based on age
+p_age(Age, 9.6) :- Age =< 6.
+p_age(Age, 5.7) :- Age =< 12.
+p_age(Age, 4.2) :- Age =< 24.
+p_age(Age, 3.4) :- Age =< 36.
+
+% Phosphorus need based on training
+p_training(0, 2.8).
+p_training(Training, 5.8) :- Training >= 0.50.
+p_training(Training, 4.2) :- Training >= 0.30.
+p_training(_, 3.6).
+
+%-------------------------------------------------------------------------------
+% Magnesium need
+%-------------------------------------------------------------------------------
+mg(Id, [mg=Mg]) :-
+    horse(Id, Dict),
+    (mg_age(Dict.age, Factor) ; mg_training(Dict.trainingMj, Factor)),
+    Mg is Factor * (Dict.weight / 100).
+
+% Magnesium need based on age
+mg_age(Age, 1.2) :- Age =< 6.
+mg_age(Age, 1.7) :- Age =< 12.
+mg_age(Age, 1.6) :- Age =< 24.
+mg_age(Age, 1.5) :- Age =< 36.
+
+% Magnesium need based on training
+mg_training(0, 1.5).
+mg_training(Training, 3) :- Training >= 0.50.
+mg_training(Training, 2.3) :- Training >= 0.30.
+mg_training(_, 1.9).
